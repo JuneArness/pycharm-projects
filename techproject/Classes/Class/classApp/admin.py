a8225a5a3b4430879abc99f36910da146ec23a52
Junior parks <junior@nationwidesoftware.com>
2021-09-23 19:31:17 -0400

    changes

--- techproject/Classes/Class/classApp/admin.py
+++ techproject/Classes/Class/classApp/admin.py
@@ -4,3 +4,11 @@ from.models import djangoClasses
 
 admin.site.register(djangoClasses)
 
+from.models import Housing
+
+admin.site.register(Housing)
+
+from.models import Meal
+
+admin.site.register(Meal)
+
--- techproject/Classes/Class/classApp/models.py
+++ techproject/Classes/Class/classApp/models.py
@@ -25,11 +25,58 @@ TYPE_LENGTH = {
 """I am attempting to call on this odject and have it looped so that the fields
  allow the user to delete and add the selections to the DB"""
 
+
+
 class djangoClasses(models.Model):
     Title = models.CharField(max_length=60, default="", choices=TYPE_CHOICES)
     Course_Number = models.IntegerField(choices=TYPE_NUMBER)
     Instructor_Name = models.CharField(max_length=60, default="")
     Duration = models.FloatField(default=0, choices=TYPE_LENGTH)
+
+"""These are the choices and object # 2"""
+
+TYPE_HOUSING = {
+    ('On Campus', 'On Campus'),
+    ('Off Campus', 'Off Campus'),
+}
+TYPE_ROOMATE ={
+
+    ('Yes', 'Yes'),
+    ('NO', 'NO'),
+}
+
+
+class Housing(models.Model):
+    Title = models.CharField(max_length=60, default="", choices= TYPE_HOUSING)
+    Roommates = models.CharField(max_length=60, default="", choices= TYPE_ROOMATE)
+
+"""These are the choices and object # 3"""
+
+
+TYPE_MAIN = {
+    ('Chicken', 'Chicken'),
+    ('Beef', 'Beef'),
+    ('Pork','Pork'),
+    ('Vegetarian', 'Vegetarian'),
+}
+
+TYPE_DRINKS = {
+    ('Water', 'Water'),
+    ('Milk', 'Milk'),
+    ('Juice', 'Juice'),
+    ('Soda', 'Soda'),
+}
+
+TYPE_SIDES = {
+    ('Salad', 'Salad'),
+    ('Mac and Cheese', 'Mac and Cheese'),
+    ('Vegetable', 'Vegetable'),
+}
+
+class Meal(models.Model):
+    Main = models.CharField(max_length=60, default="", choices=TYPE_MAIN)
+    Drinks = models.CharField(max_length=60, default="", choices=TYPE_DRINKS)
+    Sides = models.CharField(max_length=60, default="", choices=TYPE_SIDES)
 
     objects = models.Manager()
 
