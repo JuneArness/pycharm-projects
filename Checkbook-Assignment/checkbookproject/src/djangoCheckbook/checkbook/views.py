10ebc26a90ae8db043dad8333d68d3c3247fe973
Junior parks <junior@nationwidesoftware.com>
2021-09-30 18:36:22 -0400

    changes

--- Checkbook-Assignment/checkbookproject/src/djangoCheckbook/checkbook/views.py
+++ Checkbook-Assignment/checkbookproject/src/djangoCheckbook/checkbook/views.py
@@ -7,7 +7,7 @@ def home(request):
     if request.method == 'POST':
         pk = request.POST['account']
         return balance(request, pk)
-    content = {'form':form}
+    content = {'form': form}
     return render(request, 'checkbook/index.html', content)
 
 
@@ -16,8 +16,8 @@ def create_account(request):
 
 
 def balance(request, pk):
-    account = get_object_or_404(Account, pk=pk)
-    transactions = Transaction.Transactions.filter(account =pk)
+    account = get_object_or_404(Account, pk = pk)
+    transactions = Transaction.Transactions.filter(account = pk)
     current_total = account.initial_deposit
     table_contents = { }
     for t in transactions:
@@ -27,12 +27,17 @@ def balance(request, pk):
         else:
             current_total -= t.amount
             table_contents.update({t : current_total})
-    content = {'account' : account}
+    content = {'account' : account, 'table_content': table_content,'balance': current_total}
     return render(request, 'checkbook/BalanceSheet.html', content)
 
 
 def transaction(request):
-    return render(request, 'checkbook/AddTransaction.html')
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            return redirect('index')
+        content = {'form': form}
+    return render(request, 'checkbook/AddTransaction.html', content)
 
 def create_account(request):
     form = AccountForm(data=request.POST or None)
