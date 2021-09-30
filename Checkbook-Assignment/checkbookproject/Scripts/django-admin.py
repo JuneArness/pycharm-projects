b62964701e6961bdd458e567493cdfe2d703465a
Junior parks <junior@nationwidesoftware.com>
2021-09-30 15:52:00 -0400

    changes

---
+++ Checkbook-Assignment/checkbookproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\junio\OneDrive\Documents\GitHub\pycharm-projects\checkbookproject\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
